dde30f1dd74c7bd9fb9af5c7313305dee4e2b3db
JayJay <109452189+JongJunLim@users.noreply.github.com>
2023-06-22 12:13:37 +0900

    Update Pivot_and_Unpivot.sql

--- Pivot_and_Unpivot.sql
+++ Pivot_and_Unpivot.sql
@@ -1,10 +1,3 @@
-SELECT Col1, Col2, ...
-	FROM 테이블/인라인 뷰
-   PIVOT (aggregate_funtsion(arg) [, aggregate_function(arg2),...]
-   		FOR Col1 [,Col2, ...]
-		 IN (값1 [, 값2,...)
-		 );
-
 SELECT *
 	FROM (SELECT a.DEPTNO, a.SAL FROM EMP a) a
 	PIVOT (sum(a.SAL)
