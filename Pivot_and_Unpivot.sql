e45c5a411f9020715318963e7bca9420205ce70c
JayJay <109452189+JongJunLim@users.noreply.github.com>
2023-06-22 22:53:48 +0900

    Add files via upload

--- Pivot_and_Unpivot.sql
+++ Pivot_and_Unpivot.sql
@@ -1,9 +1,12 @@
+
 -- PIVOT 절
 -- Oracle 11g 버전부터 사용 가능
 -- FROM 절과 WHERE 절 사이에 기술한다.
 	-- aggregate_funtion(arg)에는 PIVOT 결과로 출력할 값을 지정
 	-- FOR 절에는 PIVOT 기준 컬럼을 지정
 	-- IN 절에는 PIVOT 기준 컬럼의 값을 지정
+
+
 SELECT Col1, Col2, ...
 	FROM 테이블/인라인 뷰
    PIVOT (aggregate_funtsion(arg) [, aggregate_function(arg2),...]
