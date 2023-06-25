3fc105e85c02ba714abfe49e9822068fb20e2a43
JayJay <109452189+JongJunLim@users.noreply.github.com>
2023-06-25 22:17:30 +0900

    Update and rename 06.LAG.sql to 06.LAG_and_LEAD.sql

--- 06.LAG_and_LEAD.sql
+++ 06.LAG_and_LEAD.sql
@@ -1,3 +1,44 @@
+--오라클에서 이전 행의 값을 찾거나 다음 행의  값을 찾기 위해서는 LAG, LEAD 함수를 사용하면 된다.
+	--LAG(expr [,offset] [,default]) OVER([partition_by_clause] order_by_clause)
+	--LEAD(expr [,offset] [,default]) OVER([partition_by_clause] order_by_clause)
+		--LAG 함수 : 이전 행의 값을 리턴
+		--LEAD 함수 : 다음 행의 값을 리턴
+		--expr : 대상 컬럼명
+		--offset : 값을 가져올 행의 위치 기본값은 1, 생략가능
+		--default : 값이 없을 경우 기본값, 생략가능
+		--partition_by_clause : 그룹 컬럼명, 생략가능
+		--order_by_clause : 정렬 컬럼명, 필수
+
+--기본 사용법
+SELECT empno
+     , ename
+     , job
+     , sal
+     , LAG(empno) OVER(ORDER BY empno)  AS empno_prev
+     , LEAD(empno) OVER(ORDER BY empno) AS empno_next
+  FROM emp 
+ WHERE job IN ('MANAGER', 'ANALYST', 'SALESMAN')
+
+--현재 행 기준으로 두번째 이전 값을 표시
+SELECT empno
+     , ename
+     , job
+     , sal
+     , LAG(empno, 2) OVER(ORDER BY empno) AS empno_prev
+  FROM emp 
+ WHERE job IN ('MANAGER', 'ANALYST', 'SALESMAN')
+
+--가져올 행이 없는 경우 기본값(9999)를 가져온다
+SELECT empno
+     , ename
+     , job
+     , sal
+     , LAG(empno, 2, 9999) OVER(ORDER BY empno) AS empno_prev
+  FROM emp 
+ WHERE job IN ('MANAGER', 'ANALYST', 'SALESMAN')
+
+	
+-- 프로젝트 사례 적용 Test
 CREATE TABLE lag_test(
    Createddate DATE,
    category VARCHAR2(20),
